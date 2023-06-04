5a5f569554bbe26ab51e5df9108159eee0a167cc
Aarón Negrín <demenus.dev@gmail.com>
2023-06-04 11:04:09 +0100

    refactor: 💡 Improvin proto format and class structure
    With these changes the problems detected in then structure has been
    mitigated (not solved yet), so at this moment showing objects (indexed
    or not) with vertex color has been fixed

---
+++ src/commonMain/kotlin/zernikalos/components/mesh/ZBufferKey.kt
@@ -0,0 +1,19 @@
+package zernikalos.components.mesh
+
+import kotlinx.serialization.Serializable
+import zernikalos.Types
+import zernikalos.ZRenderingContext
+import zernikalos.components.ZComponent
+
+@Serializable
+class ZBufferKey(private val id: Int, private val dataType: Types, private val size: Int, val count: Int, private val normalized: Boolean, private val offset: Int, private val stride: Int): ZComponent() {
+
+    override fun initialize(ctx: ZRenderingContext) {
+        ctx.enableVertexAttrib(id)
+        ctx.vertexAttribPointer(id, size, dataType.value, normalized, stride, offset)
+    }
+
+    override fun render(ctx: ZRenderingContext) {
+    }
+
+}
