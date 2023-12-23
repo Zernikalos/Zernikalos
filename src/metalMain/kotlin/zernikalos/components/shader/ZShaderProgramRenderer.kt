f3d795f007a694ba539dbf5ba114a8a94e43e8b2
Aarón <demenus.dev@gmail.com>
2023-12-23 15:27:51 +0000

    feat: 🎸 Basic support for shaders from the engine on metal

--- src/metalMain/kotlin/zernikalos/components/shader/ZShaderProgramRenderer.kt
+++ src/metalMain/kotlin/zernikalos/components/shader/ZShaderProgramRenderer.kt
@@ -1,7 +1,10 @@
 package zernikalos.components.shader
 
-import platform.Metal.MTLBufferProtocol
-import platform.Metal.MTLResourceStorageModeShared
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.ObjCObjectVar
+import platform.Foundation.NSError
+import platform.Metal.*
 import zernikalos.context.ZMtlRenderingContext
 import zernikalos.context.ZRenderingContext
 import zernikalos.components.ZComponentRender
@@ -10,7 +13,16 @@ actual class ZShaderProgramRenderer actual constructor(ctx: ZRenderingContext, d
 
     var uniformBuffer: MTLBufferProtocol? = null
 
+    lateinit var library: MTLLibraryProtocol
+    lateinit var vertexShader: MTLFunctionProtocol
+    lateinit var fragmentShader: MTLFunctionProtocol
+
     actual override fun initialize() {
+        initializeShader()
+        initializeUniformBuffer()
+    }
+
+    private fun initializeUniformBuffer() {
         ctx as ZMtlRenderingContext
 
         data.uniforms.values.forEach { uniform ->
@@ -23,6 +35,18 @@ actual class ZShaderProgramRenderer actual constructor(ctx: ZRenderingContext, d
         uniformBuffer?.label = "UniformBuffer"
     }
 
+    @OptIn(ExperimentalForeignApi::class)
+    private fun initializeShader() {
+        ctx as ZMtlRenderingContext
+
+        var err: CPointer<ObjCObjectVar<NSError?>>? = null
+
+        library = ctx.device.newLibraryWithSource(shaderSource, MTLCompileOptions(), err)!!
+
+        vertexShader = library.newFunctionWithName("vertexShader")!!
+        fragmentShader = library.newFunctionWithName("fragmentShader")!!
+    }
+
     actual override fun bind() {
     }
 
@@ -30,3 +54,62 @@ actual class ZShaderProgramRenderer actual constructor(ctx: ZRenderingContext, d
     }
 
 }
+
+const val shaderSource = """
+#include <metal_stdlib>
+#include <simd/simd.h>
+
+// Including header shared between this Metal shader code and Swift/C code executing Metal API commands
+// #import "ShaderTypes.h"
+
+using namespace metal;
+
+typedef struct
+{
+    float3 position [[attribute(1)]];
+    float3 color [[attribute(2)]];
+    //float2 texCoord [[attribute(1)]];
+} Vertex;
+
+typedef struct
+{
+    float4 position [[position]];
+    float3 color;
+} ColorInOut;
+
+typedef struct
+{
+    matrix_float4x4 mvpMatrix;
+    // matrix_float4x4 mvpMatrix2;
+
+//    matrix_float4x4 viewMatrix;
+//    matrix_float4x4 modelMatrix;
+} Uniforms;
+
+vertex ColorInOut vertexShader(Vertex in [[stage_in]],
+                               constant Uniforms & uniforms [[ buffer(7) ]])
+{
+    ColorInOut out;
+
+    float4 position = float4(in.position, 1.0);
+    out.position = uniforms.mvpMatrix * position;
+    out.color = in.color;
+    // out.texCoord = float2(1.0, 0.0);
+
+    return out;
+}
+
+fragment float4 fragmentShader(ColorInOut in [[stage_in]],
+                               constant Uniforms & uniforms [[ buffer(7) ]],
+                               texture2d<half> colorMap     [[ texture(0) ]])
+{
+//    constexpr sampler colorSampler(mip_filter::linear,
+//                                   mag_filter::linear,
+//                                   min_filter::linear);
+//
+//    half4 colorSample   = colorMap.sample(colorSampler, in.texCoord.xy);
+
+    return float4(in.color, 1.0);
+}
+
+"""
